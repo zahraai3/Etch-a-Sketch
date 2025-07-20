ec341013743396e1332a5d184e87914e64bf75e1
zahraai3 <zahraaibrahem241@gmail.com>
2025-07-20 22:28:03 +0300

    Add dynamic 16x16 grid with hover color change and grid resizing via button

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const container = document.querySelector('.container');
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'pink','grey','light blue','dark yellow','purple'];
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'aliceblue','peachpuff','lightblue','darkyellow','purple']; 
+
 
 for (let i=0;i<256;i++)
 {
     const square=document.createElement('div');
     square.classList.add('square');
 
-    square.style.backgroundColor = colors[i % colors.length];
+    let colorIndex =i;
+
+    square.addEventListener('mouseenter',()=>{
+        square.style.backgroundColor = colors[colorIndex % colors.length];
+    });
     container.appendChild(square);
 
 }
+
+const button =document.querySelector('button')
+button.addEventListener('click',()=>{
+    const squaresPerSide = prompt("Enter the number of squares per side (max 100):");
+    let num = Number(squaresPerSide);
+
+    if (num >100){
+        num=100;
+    }
+    else if (num < 1|| isNaN(num)){
+        alert("please enter a valid number")
+        return;
+    }
+    
+        container.innerHTML='';
+        const size = 500/num;
+
+        for (let i = 0; i < num * num; i++) {
+
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+    square.style.width = `${size}px`;
+  square.style.height = `${size}px`;
+
+  let colorIndex =i;
+
+    square.addEventListener('mouseenter',()=>{
+        square.style.backgroundColor = colors[colorIndex % colors.length];
+    });
+
+  container.appendChild(square);
+}
+
+});
